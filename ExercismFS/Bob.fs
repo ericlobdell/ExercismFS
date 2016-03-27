59d203870da7dbfd78506370181ae1ad65b37bb8
Eric Lobdell <eric@ericlobdell.me>
2016-03-27 19:24:52 -0400

    finished Bob

--- ExercismFS/Bob.fs
+++ ExercismFS/Bob.fs
@@ -1,14 +1,31 @@
 ﻿module ExercismFS
 
 open System
+open System.Text.RegularExpressions
 
 let IsAllCaps (s:string) = String.Equals( s, s.ToUpper())
-let IsQuestion (s:string) = s.EndsWith("?")
+
+let IsEmpty (s:string) = String.IsNullOrWhiteSpace(s)
+
+let IsAllNumbers (s:string) =
+    let rgx = new Regex(@"[\s,?!]")
+    let stripped = rgx.Replace(s, "") 
+    let result: int = 0
+    Int32.TryParse(stripped, ref result)
+
+let IsExclaimation (s:string) : bool =
+   ( IsAllCaps s ) && not ( IsAllNumbers s )
+
+let IsQuestion (s:string) = 
+    s.EndsWith("?") && not (IsExclaimation s)
+
+
 
 type Bob( phrase : string ) = 
+    let sut = phrase.Trim()
 
     member this.hey() =
-        "Whatever."
-
-
-    
+        if IsEmpty sut then "Fine. Be that way!"
+        else if IsQuestion sut then "Sure."
+        else if IsExclaimation sut then "Whoa, chill out!"
+        else "Whatever."
