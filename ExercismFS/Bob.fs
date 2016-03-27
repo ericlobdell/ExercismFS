5bb05b6b4232b042b001bcdaa0724aa56ab26c7c
Eric Lobdell <eric@ericlobdell.me>
2016-03-27 18:29:49 -0400

    initial commit

---
+++ ExercismFS/Bob.fs
@@ -0,0 +1,14 @@
+﻿module ExercismFS
+
+open System
+
+let IsAllCaps (s:string) = String.Equals( s, s.ToUpper())
+let IsQuestion (s:string) = s.EndsWith("?")
+
+type Bob( phrase : string ) = 
+
+    member this.hey() =
+        "Whatever."
+
+
+    
