560ee80d6da33ba67aa268f6ec60479ba94ee68d
Eric Lobdell <eric@ericlobdell.me>
2016-07-30 18:09:40 -0400

    finished difference of squares

---
+++ ExercismFS/DifferenceOfSquares.fs
@@ -0,0 +1,14 @@
+﻿module DifferenceOfSquares
+
+let square n = n * n
+
+let squareOfSums n =
+    [1..n] |> List.sum |> square
+
+let sumOfSquares n =
+    [1..n] |> List.map square |> List.sum
+
+
+let difference n =
+    (squareOfSums n) - (sumOfSquares n)
+
