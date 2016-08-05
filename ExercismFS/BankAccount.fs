2cffbf2e5ced7d5208e0b38fb1a19e0ac624a99b
Eric Lobdell <eric@ericlobdell.me>
2016-08-04 21:14:22 -0400

    finished BankAccount

---
+++ ExercismFS/BankAccount.fs
@@ -0,0 +1,28 @@
+﻿module BankAccount
+
+type Account = {
+    Balance: double
+    IsOpen: bool
+}
+
+let getBalance account =
+    if account.IsOpen then Some(account.Balance) else None
+
+let mkBankAccount() = 
+    { Balance = 0.0; IsOpen = false }
+
+let openAccount account =
+    { Balance = account.Balance; IsOpen = true }
+
+let closeAccount account =
+    { Balance = account.Balance; IsOpen = false }
+
+let updateBalance amount account =
+    { Balance = account.Balance + amount; IsOpen = account.IsOpen }
+
+
+
+
+
+
+    
